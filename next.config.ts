4caf2d1fa2c5bcfe9c8ef2b0d36df5d200da69d1
Janrhick Pucio <janrhickpucio141@gmail.com>
2025-11-21 08:05:15 +0800

    commit change

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,18 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'spacexpatchlist.space',
+      },
+      {
+        protocol: 'http',
+        hostname: 'spacexpatchlist.space',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
