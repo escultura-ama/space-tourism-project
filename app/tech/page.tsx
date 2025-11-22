c21bacc1d64ecff892da68edfa2197246774ac0f
Ryugasakii <183824156+Riogasakii@users.noreply.github.com>
2025-11-23 06:54:56 +0800

    commit: desktop

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -115,8 +115,8 @@ export default function Technology() {
             </div>
           </div>
 
-          <div className="hidden lg:block lg:static lg:top-1/2 lg:right-0 lg:-translate-y-1/2">
-            {tech.imageSrc ? (
+          <div className="hidden lg:block lg:absolute lg:top-1/2 lg:right-0 lg:-translate-y-1/2">
+            {tech.imageSrc && (
               <Image
                 src={tech.imageSrc}
                 alt={tech.name}
