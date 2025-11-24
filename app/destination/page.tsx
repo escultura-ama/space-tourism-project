6176508405c4c1d9cd138ebd504da93544159820
Bryan2004-sudo <gavinabryan64@gmail.com>
2025-11-24 16:37:41 +0800

    ADD TEXT COLOR

--- app/destination/page.tsx
+++ app/destination/page.tsx
@@ -4,7 +4,8 @@ import { useState } from 'react';
 import Image from 'next/image';
 import { destinations } from './data';
 
-const SKY_BLUE_COLOR = 'rgb(135, 206, 235)'; 
+// Updated: Use the solid RGB color value, as the CSS variable won't work in inline style
+const PRIMARY_TEXT_COLOR = 'rgb(208, 214, 249)';
 
 function Destination() {
   const [activeDestinationIndex, setActiveDestinationIndex] = useState(0);
@@ -22,12 +23,12 @@ function Destination() {
         </div>
         <div className="lg:items-start lg:flex-1 flex flex-col justify-between">
           <div className="px-8 text-center lg:text-left md:mt-8 lg:mt-0 lg:px-0 lg:top-0">
-            {/* DESTINATION BUTTONS (Moon, Mars, Europa, Titan) color is SKY BLUE */}
-            <div className="barlow-condensed tracking-wider inline-flex gap-6 my-4 lg:my-0" style={{ color: SKY_BLUE_COLOR }}>
+            {/* DESTINATION BUTTONS (Moon, Mars, Europa, Titan) color is rgb(208, 214, 249) */}
+            <div className="barlow-condensed tracking-wider inline-flex gap-6 my-4 lg:my-0" style={{ color: PRIMARY_TEXT_COLOR }}>
               {destinations.map((dest, index) => (
-                <button 
-                  key={dest.name} 
-                  onClick={() => setActiveDestinationIndex(index)} 
+                <button
+                  key={dest.name}
+                  onClick={() => setActiveDestinationIndex(index)}
                   className={`uppercase cursor-pointer hover:text-gray-400 hover:underline hover:underline-offset-8 transition duration-150 ease-in-out ${activeDestinationIndex === index ? 'text-white underline underline-offset-8' : ''}`}
                 >
                   {dest.name}
@@ -36,20 +37,20 @@ function Destination() {
             </div>
             <h2 className="Bellefair text-6xl text-white my-4 uppercase lg:text-[100px] lg:mt-10">{activeDestination.name}</h2>
             
-            {/* DESTINATION DESCRIPTION text color is now SKY BLUE */}
-            <p className="text-base barlow md:px-24 lg:px-0 lg:w-2/3" style={{ color: SKY_BLUE_COLOR }}>{activeDestination.description}</p>
+            {/* DESTINATION DESCRIPTION text color is rgb(208, 214, 249) */}
+            <p className="text-base barlow md:px-24 lg:px-0 lg:w-2/3" style={{ color: PRIMARY_TEXT_COLOR }}>{activeDestination.description}</p>
             
             <hr className="border-[#383B4B] h-0.5 mt-6 lg:mt-12" />
           </div>
           <div className="mt-8 lg:mt-0 text-center md:flex align-top items-center justify-center md:justify-around md:px-16 lg:items-start lg:justify-items-start lg:justify-start lg:px-0 lg:mb-4 lg:text-left">
-            {/* AVG. DISTANCE label color is SKY BLUE */}
+            {/* AVG. DISTANCE label color is rgb(208, 214, 249) */}
             <div className="uppercase lg:mr-20">
-              <p className="barlow-condensed tracking-wider mb-1" style={{ color: SKY_BLUE_COLOR }}>Avg. Distance</p>
+              <p className="barlow-condensed tracking-wider mb-1" style={{ color: PRIMARY_TEXT_COLOR }}>Avg. Distance</p>
               <p className="Bellefair text-3xl text-white">{activeDestination.distance}</p>
             </div>
-            {/* EST. TRAVEL TIME label color is SKY BLUE */}
+            {/* EST. TRAVEL TIME label color is rgb(208, 214, 249) */}
             <div className="uppercase mt-4 md:mt-0">
-              <p className="barlow-condensed tracking-wider mb-1" style={{ color: SKY_BLUE_COLOR }}>Est. travel time</p>
+              <p className="barlow-condensed tracking-wider mb-1" style={{ color: PRIMARY_TEXT_COLOR }}>Est. travel time</p>
               <p className="Bellefair text-3xl text-white">{activeDestination.travel}</p>
             </div>
           </div>
