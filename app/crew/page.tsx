899b187d5dd5177e605edf8900ed9ed3f16390d7
haigarcia08 <haidiegarcia.24@gmail.com>
2025-11-24 13:52:18 +0800

    commit: update 90% similarity to the reference

--- app/crew/page.tsx
+++ app/crew/page.tsx
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 
 function Crew() {
   return (
@@ -6,3 +7,27 @@ function Crew() {
 }
 
 export default Crew
+=======
+"use client";
+
+import CrewCarousel from "../../components/CrewCarousel";
+
+export default function CrewPage() {
+  return (
+    <div
+      className="
+        min-h-screen pt-24 pb-8
+        bg-cover bg-no-repeat bg-center
+        bg-[url('/images/bg/crew-bg.png')]
+        lg:bg-[url('/images/bg/lg-crew-bg.png')]
+        text-white
+      "
+    >
+      
+
+        <CrewCarousel />
+      </div>
+    
+  );
+}
+>>>>>>> Stashed changes
